cd2a34812e2bc37b3812ceaffb78f619ad2319e3
jorgehq <jor2018qw@gmail.com>
2024-12-15 16:31:10 -0300

    PrimerCommit 3

--- src/main/java/Domain/Colaborador/MedioDeContacto/Telegram.java
+++ src/main/java/Domain/Colaborador/MedioDeContacto/Telegram.java
@@ -20,36 +20,12 @@ public class Telegram{
   public Telegram() {
 
   }
-/*
-  private static final String BOT_TOKEN = "7523722721:AAG3Mzr2XnBhvbAu2YgUg2o7Oit1RHX1y24";
-  private static final String CHAT_ID = "5475107169";
+
+
 
 
   public void enviarMensaje(String contacto,String cuerpo) {
-    try {
-      String urlString = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage";
-      URL url = new URL(urlString);
-      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestMethod("POST");
-      connection.setDoOutput(true);
-      connection.setRequestProperty("Content-Type", "application/json");
 
-      // Constructing the JSON input string
-      String jsonInputString = "{\"chat_id\":\"" + CHAT_ID + "\", \"text\":\"" + cuerpo + "\"}";
-
-      // Sending the request
-      try (OutputStream os = connection.getOutputStream()) {
-        byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
-        os.write(input, 0, input.length);
-      }
-
-      int responseCode = connection.getResponseCode();
-      System.out.println("Response Code: " + responseCode);
-
-      connection.disconnect();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
-*/
+
 }
